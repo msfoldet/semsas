e6356cabf15762aa78d9dce984ec27a2b9ceb0fc
msfoldet <alnasrb705@gmail.com>
2024-11-14 15:38:54 +0300

    Update page.jsx

--- ‏‏my-app/src/app/aram/page.jsx
+++ ‏‏my-app/src/app/aram/page.jsx
@@ -33,7 +33,7 @@ const Page = () => {
   const PostToDiscord = () => {
     const description = Object.entries(form.data)
       .map((d) => `${d[0]} : ${d[1]} `)
-      .join("\n");
+      .join("%0A");
     Send(description)
     
   };
